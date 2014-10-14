edaf285324fef976e60debdc6f86085d7f664d5b
Manuel Hieke <mahieke@htwg-konstanz.de>
2014-10-14 18:26:27 +0200

    adjust classes with the new ConfiLoader

--- help4tutor/exercisedownloader.py
+++ help4tutor/exercisedownloader.py
@@ -3,64 +3,66 @@ import subprocess
 import shutil
 import os
 from colorama import Fore, Back, Style
+import configloader as ldr
 
 
 class ExerciseDownloader():
-	"""
-	TODO:
+    """
+    TODO:
 
-	:author: Lars Eckervogt
-	"""
+    :author: Lars Eckervogt
+    """
 
-	def __init__(self, config_file, group, exercise, dest_dir):
-		"""
-
-		:param config_file:
-		:param group:
-		:param exercise:
-		:param dest_dir:
-		:return:
-		"""
-		self.config_file = config_file
-		self.group = group
-		self.exercise = exercise
-		self.dest_path = '%s%s/' % (dest_dir, exercise)
-		self.groups = ''
-
-	def loadConfig(self):
-		config = configparser.ConfigParser()
-		if os.path.isfile(self.config_file):
-			config.read(self.config_file)
-		else:
-			return False
-
-		self.url = config['DEFAULT']['git_url']
-		self.groups = config['DEFAULT']['group_%s' % self.group].split(',')
-		self.git_group_name = config['DEFAULT']['git_group_name']
-		self.git_repo_prefix = config['DEFAULT']['git_repo_prefix']
-
-	def clean(self):
-		if os.path.exists(self.dest_path):
-			raw_input(Back.RED + Style.BRIGHT + 'This operation will clean the destination directory: {0:s}'.format(
-				self.dest_path) + Style.RESET_ALL + '\n' + Back.RED + Style.BRIGHT
-				+ 'Press <Enter> to continue (CTRL-C to abort):' + Style.RESET_ALL)
-
-		shutil.rmtree(self.dest_path)
+    conf_props = ['git_url']
+    conf_section = 'Default'
 
 
-	def download(self):
-		self.loadConfig()
-		self.clean()
+    def __init__(self, config_file, group, exercise, dest_dir):
+        """
 
-		for group in self.groups:
-			cmd_checkout = 'git clone git@{0:s}:{1:s}/{2:s}{3:s}.git {4:s}{2:s}{3:s}'.format(
-				self.url.replace('https://', ''), self.git_group_name, self.git_repo_prefix, group, self.dest_path)
-			subprocess.call([cmd_checkout], shell=True)
+        :param config_file:
+        :param group:
+        :param exercise:
+        :param dest_dir:
+        :return:
+        """
+        self.config_file = config_file
+        self.group = group
+        self.exercise = exercise
+        self.dest_path = '%s%s/' % (dest_dir, exercise)
+        self.groups = ''
 
-			cmd_get_tags = 'cd  %s%s%s; git log --tags=%s* --simplify-by-decoration --pretty="format:%%ci %%d"' % ( \
-				self.dest_path, self.git_repo_prefix, group, self.exercise)
-			subprocess.call([cmd_get_tags], shell=True)
+    def loadConfig(self):
+        loader = ldr.ConfigLoader(self.config_file)
+        config = loader.loadConfig(self.conf_props, self.conf_section)
 
-			tag = input('Select tag:')
-			cmd_checkout_tag = 'cd  %s%s%s; git checkout tags/%s' % (self.dest_path, self.git_repo_prefix, group, tag)
-			subprocess.call([cmd_checkout_tag], shell=True)
+        self.url = config['git_url']
+        self.groups = config['group_%s' % self.group].split(',')
+        self.git_group_name = config['git_group_name']
+        self.git_repo_prefix = config['git_repo_prefix']
+
+    def clean(self):
+        if os.path.exists(self.dest_path):
+            raw_input(Back.RED + Style.BRIGHT + 'This operation will clean the destination directory: {0:s}'.format(
+                self.dest_path) + Style.RESET_ALL + '\n' + Back.RED + Style.BRIGHT
+                + 'Press <Enter> to continue (CTRL-C to abort):' + Style.RESET_ALL)
+
+        shutil.rmtree(self.dest_path)
+
+
+    def download(self):
+        self.loadConfig()
+        self.clean()
+
+        for group in self.groups:
+            cmd_checkout = 'git clone git@{0:s}:{1:s}/{2:s}{3:s}.git {4:s}{2:s}{3:s}'.format(
+                self.url.replace('https://', ''), self.git_group_name, self.git_repo_prefix, group, self.dest_path)
+            subprocess.call([cmd_checkout], shell=True)
+
+            cmd_get_tags = 'cd  %s%s%s; git log --tags=%s* --simplify-by-decoration --pretty="format:%%ci %%d"' % ( \
+                self.dest_path, self.git_repo_prefix, group, self.exercise)
+            subprocess.call([cmd_get_tags], shell=True)
+
+            tag = input('Select tag:')
+            cmd_checkout_tag = 'cd  %s%s%s; git checkout tags/%s' % (self.dest_path, self.git_repo_prefix, group, tag)
+            subprocess.call([cmd_checkout_tag], shell=True)
--- help4tutor/issueuploader.py
+++ help4tutor/issueuploader.py
@@ -1,12 +1,13 @@
 import gitlab as git
 import os
 import logging as log
-import configparser
+import configloader as ldr
+
 from getpass import *
 
 
 class IssueUploader():
-	"""
+    """
 	Class which uploads the content of files [name].md in the src_dir to a
 	gitlab server with repositories the name [NAME] as issue. Eg. group1.md
 	would be uploaded to repo GROUP1.
@@ -14,8 +15,11 @@ class IssueUploader():
 	:author: Lars Eckervogt
 	"""
 
-	def __init__(self, config_file, prefix, exercise, src_dir):
-		"""
+    conf_props = ['git_url']
+    conf_section = 'Default'
+
+    def __init__(self, config_file, prefix, exercise, src_dir):
+        """
 
 		:param url:
 		:param prefix:
@@ -24,45 +28,51 @@ class IssueUploader():
 		:return:
 		"""
 
-		self.config_file = config_file
-		self.prefix = prefix
-		self.exercise = exercise
-		self.src_dir = src_dir
-        self.config_props = []
+        self.config_file = config_file
+        self.prefix = prefix
+        self.exercise = exercise
+        self.src_dir = src_dir
 
-	def getToken(self):
-		return getpass('Please provide private token from Gitlab (' + self.url + '):')
+        loader = ldr.ConfigLoader(self.config_file)
+        config = loader.loadConfig(self.conf_props, self.conf_section)
 
-	def upload(self):
-		self.loadConfig()
-		gl = git.Gitlab(self.url, self.getToken(), verify_ssl=False)
+        self.url = config['git_url']
 
-		if gl:
-			result_files = os.listdir(self.src_dir)
-			result_groups = []
 
-			for file in result_files:
-				group = file.replace('.md', '').upper()
-				result_groups.append(group)
-				log.info('Added %s to the groups that have a ne result' % group)
 
-			for project in gl.getprojects(per_page=100):
-				if project['name'] in result_groups:
-					project_id = project['id']
+    def getToken(self):
+        return getpass('Please provide private token from Gitlab (' + self.url + '):')
 
-					log.info('Found project %s with id %s' % (project['name'], str(project['id'])))
+    def upload(self):
 
-					title = '%s %s' % (self.prefix, self.exercise)
-					label = '%s, %s' % (self.prefix, self.exercise)
+        gl = git.Gitlab(self.url, self.getToken(), verify_ssl=False)
 
-					content = ''
-					with open(self.src_dir + str(project['name']).lower() + '.md', 'r') as content_file:
-						content = content_file.read()
+        if gl:
+            result_files = os.listdir(self.src_dir)
+            result_groups = []
 
-					log.info('Creating issue for %s (ID:%s)' % (project['name'], str(project['id'])))
-					log.info('\t -> Title: %s ' % title)
-					log.info('\t -> Labels: %s ' % label)
+            for file in result_files:
+                group = file.replace('.md', '').upper()
+                result_groups.append(group)
+                log.info('Added %s to the groups that have a ne result' % group)
 
-					input('Press <Enter> to continue (CTRL-C to abort):')
+            for project in gl.getprojects(per_page=100):
+                if project['name'] in result_groups:
+                    project_id = project['id']
 
-					gl.createissue(id_=project_id, title=title, description=content, labels=label)
+                    log.info('Found project %s with id %s' % (project['name'], str(project['id'])))
+
+                    title = '%s %s' % (self.prefix, self.exercise)
+                    label = '%s, %s' % (self.prefix, self.exercise)
+
+                    content = ''
+                    with open(self.src_dir + str(project['name']).lower() + '.md', 'r') as content_file:
+                        content = content_file.read()
+
+                    log.info('Creating issue for %s (ID:%s)' % (project['name'], str(project['id'])))
+                    log.info('\t -> Title: %s ' % title)
+                    log.info('\t -> Labels: %s ' % label)
+
+                    input('Press <Enter> to continue (CTRL-C to abort):')
+
+                    gl.createissue(id_=project_id, title=title, description=content, labels=label)
